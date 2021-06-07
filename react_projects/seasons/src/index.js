6272d7621649b3c6f0fcecd8bf5a135f41659dd6
Jared Brown <jaredbrown1@hotmail.com>
2021-06-07 12:08:37 -0700

    state being updated for state practice

---
+++ react_projects/seasons/src/index.js
@@ -0,0 +1,26 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+class App extends React.Component {
+	constructor(props) {
+		super(props);
+
+		// this is the only time we do direct assignment to this.state
+		this.state = { lat: null };
+
+		window.navigator.geolocation.getCurrentPosition(
+			(position) => {
+				// we called setstate to update the state
+				console.log(position);
+				this.setState({ lat: position.coords.latitude });
+			},
+			(err) => console.log(err)
+		);
+	}
+
+	render() {
+		return <div>Latitude: {this.state.lat} </div>;
+	}
+}
+
+ReactDOM.render(<App />, document.querySelector("#root"));
